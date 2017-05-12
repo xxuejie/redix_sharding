eb339bc60c9939f2317ee73d12c2ecb335e0dfb5
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2017-05-12 14:10:16 +0800

    Update mix file with package info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule RedixSharding.Mixfile do
   use Mix.Project
 
+  @description "A wrapper of Redix with sharding & pooling support"
+
+  @repo_url "https://github.com/xxuejie/redix_sharding"
+
+  @version "0.1.0"
+
   def project do
     [app: :redix_sharding,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+
+     # Hex
+     package: package(),
+     description: @description]
   end
 
   # Configuration for the OTP application
@@ -16,6 +26,12 @@ defmodule RedixSharding.Mixfile do
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger, :redix]]
+  end
+
+  defp package() do
+    [maintainers: ["Xuejie Xiao"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => @repo_url}]
   end
 
   # Dependencies can be Hex packages:
